21b774c81951c3f4f48b916946019a1ce75bfaeb
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-20 08:54:01 +0200

    fix: add some missing attributes

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 function validateFormName() {
-  let x = document.forms["contactForm"]["name"].value;
-  if (x == "ironhack" || x == "Ironhack") {
+  let fullName = document.forms["contactForm"]["name"].value;
+  if (fullName === "ironhack" || fullName === "Ironhack") {
     alert("You cannot be Ironhack, because I am Ironhack");
     return false;
   }
